117a9eb408df5bdad99ab1e2498c5eadc6ffde6f
Ajmal <117256595+Ajmal45@users.noreply.github.com>
2026-07-08 15:05:42 +0530

    Fix: add request logging to debug routing

--- server/app.js
+++ server/app.js
@@ -23,6 +23,12 @@ import analyticsRoutes from './routes/analyticsRoutes.js';
 
 const app = express();
 
+// Request logging (first middleware)
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+});
+
 // Security Middlewares
 app.use(helmet());
 app.use((req, res, next) => {
